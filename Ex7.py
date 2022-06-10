7f9b44d6c6057a44c735aa3820d30549416bb617
KhrenovKonstantin <etayazzaza@yandex.ru>
2022-06-10 15:41:42 +0500

    DZ №2.7

--- Ex7.py
+++ Ex7.py
@@ -4,6 +4,17 @@ method = {"method": "POST"}
 method1 = {"method": "GET"}
 method2 = {"method": "PUT"}
 method3 = {"method": "DELETE"}
+
+method_all = (method, method1, method2, method3)
+g = {}
+params = method1
+data = method
+#for i in method_all:
+   #if i == {"method": "GET"}:
+       # g == params
+    #else:
+       # g == data
+
 first_response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
 second_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=method)
 third_response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
@@ -17,45 +28,44 @@ print(third_response.text)
 print(four_response.text)
 print("-------------")
 
-if method != {"method": "GET"}:
-    method = method1
-    first_response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
-    second_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=method)
-    third_response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
-    four_response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
+method.update(method1)
 
-    print("Запрос GET")
-    print(first_response.text)
-    print(second_response.text)
-    print(third_response.text)
-    print(four_response.text)
-    print("-------------")
+first_response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
+second_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=method)
+third_response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
+four_response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
 
-if method != {"method": "PUT"}:
-    method = method2
-    first_response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
-    second_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=method)
-    third_response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
-    four_response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
+print("Запрос GET")
+print(first_response.text)
+print(second_response.text)
+print(third_response.text)
+print(four_response.text)
+print("-------------")
 
-    print("Запрос PUT")
-    print(first_response.text)
-    print(second_response.text)
-    print(third_response.text)
-    print(four_response.text)
-    print("-------------")
+method.update(method2)
 
-if method != {"method": "delete"}:
-    method = method3
-    first_response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
-    second_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=method)
-    third_response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
-    four_response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
+first_response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
+second_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=method)
+third_response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
+four_response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
 
-    print("Запрос DELETE")
-    print(first_response.text)
-    print(second_response.text)
-    print(third_response.text)
-    print(four_response.text)
-    print("-------------")
+print("Запрос PUT")
+print(first_response.text)
+print(second_response.text)
+print(third_response.text)
+print(four_response.text)
+print("-------------")
+
+method.update(method3)
+first_response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
+second_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=method)
+third_response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
+four_response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
+
+print("Запрос DELETE")
+print(first_response.text)
+print(second_response.text)
+print(third_response.text)
+print(four_response.text)
+print("-------------")
 
