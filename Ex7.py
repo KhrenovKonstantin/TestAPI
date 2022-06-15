cdb8d64a60a0f18a4e22f182483e626b1293f972
KhrenovKonstantin <etayazzaza@yandex.ru>
2022-06-15 12:08:13 +0500

    DZ №2.7

--- Ex7.py
+++ Ex7.py
@@ -6,19 +6,20 @@ method2 = {"method": "PUT"}
 method3 = {"method": "DELETE"}
 
 method_all = (method, method1, method2, method3)
-g = {}
+
 params = method1
 data = method
-#for i in method_all:
-   #if i == {"method": "GET"}:
-       # g == params
-    #else:
-       # g == data
+for i in method_all:
+    for j in method_all:
+        if i == {"method": "GET"}:
+            params = i
+        else:
+            params = i
 
-first_response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
-second_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=method)
-third_response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
-four_response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
+first_response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=params)
+second_response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=params)
+third_response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=params)
+four_response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=params)
 
 
 print("Запрос POST")
