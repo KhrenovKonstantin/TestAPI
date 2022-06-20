00bbc88f9bc40fb7979698e5905c1eeac4fffbfb
KhrenovKonstantin <etayazzaza@yandex.ru>
2022-06-20 20:09:43 +0500

    DZ №3.13

--- 3.DZ/ex13.py
+++ 3.DZ/ex13.py
@@ -4,8 +4,8 @@ import requests
 
 class TestFirstAPI:
     users = [
-        ('Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, '
-         'like Gecko) Version/4.0 Mobile Safari/534.30'),
+        {'User-Agent': 'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, '
+         'like Gecko) Version/4.0 Mobile Safari/534.30'},
         ('Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 '
          'Mobile/15E148 Safari/604.1'),
         'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
@@ -15,18 +15,22 @@ class TestFirstAPI:
          'Version/13.0.3 Mobile/15E148 Safari/604.1')
     ]
 
-    @pytest.mark.parametrize('user_agent', users)
-    def ex13(self, user_agent):
+    @pytest.mark.parametrize('User-Agent', users)
+    def ex13(self, users):
         url = 'https://playground.learnqa.ru/ajax/api/user_agent_check'
-        data = {'user_agent': user_agent}
+        data = {'User-Agent': users}
 
         response = requests.get(url, headers=data)
 
         assert response.status_code == 200, 'Wrong code'
 
+        platform = response.json()['platform']
+        browser = response.json()['browser']
+        device = response.json()['device']
+
         response_dict = response.json()
-        assert "platform" in response_dict, 'platform not found'
-        assert "browser" in response_dict, 'browser not found'
-        assert "device" in response_dict, 'device not found'
+        assert "platform" in platform, 'platform not found'
+        assert "browser" in browser, 'browser not found'
+        assert "device" in device, 'device not found'
 
         print(response.text)
